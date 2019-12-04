cc0297ed740f50ed860df51abbfe87910a5f60e5
Neil Richardson <nrich@neiltopia.com>
2019-12-04 18:36:23 +0800

    Add use button back to protocol

--- Quake/cl_input.c
+++ Quake/cl_input.c
@@ -382,6 +382,10 @@ void CL_SendMove (const usercmd_t *cmd)
 		bits |= 2;
 	in_jump.state &= ~2;
 
+	if (in_use.state & 3)
+		bits |= 4;
+	in_use.state &= ~2;
+
 	MSG_WriteByte (&buf, bits);
 
 	MSG_WriteByte (&buf, in_impulse);
